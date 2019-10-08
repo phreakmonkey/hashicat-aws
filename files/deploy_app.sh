5d7aab12fbe7d91a8705634f09c371cdf719f36b
K.C. Budd <phreakmonkey@users.noreply.github.com>
2019-10-08 15:14:36 -0700

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -10,8 +10,8 @@ cat << EOM > /var/www/html/index.html
 
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
-  <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own. 
+  <center><h2>Goodbye World!</h2></center>
+  Welcome to ${PREFIX}'s app. KILL THEE WITH TEH CUTENESS. 
   <!-- END -->
   
   </div>
