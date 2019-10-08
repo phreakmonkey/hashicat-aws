21d948ab8148456d719ed2003daebb21befeca2f
K.C. Budd <phreakmonkey@users.noreply.github.com>
2019-10-08 15:18:07 -0700

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Goodbye World!</h2></center>
-  Welcome to ${PREFIX}'s app. KILL THEE WITH TEH CUTENESS. 
+  Welcome to ${PREFIX}'s app.<br /> KILL THEE WITH TEH CUTENESS. 
   <!-- END -->
   
   </div>
